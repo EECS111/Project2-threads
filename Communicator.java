8d37d03c890402ae930b451a388a73938ca6b988
Anoop Dhillon <anoopdhillon30@gmail.com>
2016-05-02 21:00:57 -0700

    Completed Task 4! Only finishes if you increase Grading Time

--- Communicator.java
+++ Communicator.java
@@ -1,19 +1,26 @@
 package nachos.threads;
 
 import nachos.machine.*;
+import java.util.*;
 
 /**
  * A <i>communicator</i> allows threads to synchronously exchange 32-bit
  * messages. Multiple threads can be waiting to <i>speak</i>, and multiple
  * threads can be waiting to <i>listen</i>. But there should never be a time
  * when both a speaker and a listener are waiting, because the two threads can
- * be paired off at this point.
+ * be paired oAff at this point.
  */
 public class Communicator {
 	/**
 	 * Allocate a new communicator.
 	 */
 	public Communicator() {
+		ComLock = new Lock();
+		sCond = new Condition(ComLock);
+		lCond = new Condition(ComLock);
+		sCount = 0;
+		lCount = 0;
+		wordReady = false;
 	}
 
 	/**
@@ -27,6 +34,16 @@ public class Communicator {
 	 * @param word the integer to transfer.
 	 */
 	public void speak(int word) {
+		ComLock.acquire();
+		sCount++;
+		while(lCount <= 0 || wordReady){
+			sCond.sleep();
+		}
+		this.word = word;
+		wordReady = true;
+		lCond.wakeAll();
+		sCount--;
+		ComLock.release();	
 	}
 
 	/**
@@ -36,6 +53,26 @@ public class Communicator {
 	 * @return the integer transferred.
 	 */
 	public int listen() {
-		return 0;
+		ComLock.acquire();
+		lCount++;
+		while(!wordReady){
+			sCond.wakeAll();
+			lCond.sleep();
+		}
+		int W = this.word;
+		wordReady = false;
+		
+		lCount--;
+		ComLock.release();
+		return W;
+		
 	}
+	
+	private Lock ComLock;
+	private Condition sCond;
+	private long sCount;
+	private Condition lCond;
+	private long lCount;
+	private boolean wordReady;
+	private int word;
 }
